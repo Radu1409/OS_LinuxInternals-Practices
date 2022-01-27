1c5eb79dd2beac62e9af52905dd97191fce07f25
Purecel Radu <79523935+Radu1409@users.noreply.github.com>
2022-01-27 20:25:01 +0200

    Create ex_2.sh

---
+++ bash_exercises/ex_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+<<EXERCITIU
+  Modificați scriptul shell din exercițiul 1 pentru a include o variabilă. Variabila va deține conținutul mesajului "Bine ati venit!"
+EXERCITIU
+
+TEXT=”Bine ati venit!”
+echo $TEXT
