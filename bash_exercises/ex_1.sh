4e10405d14359ab5b6257f4ab8e768359b83af33
Purecel Radu <79523935+Radu1409@users.noreply.github.com>
2022-01-27 20:28:24 +0200

    Delete bash_exercises directory

--- bash_exercises/ex_1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-<<EXERCITIU
-  Scrieți un script shell care va afisa „Bine ati venit!” pe ecran.
-EXERCITIU
-
-echo “Bine ati venit!”
-exit
--- bash_exercises/ex_2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-<<EXERCITIU
-  Modificați scriptul shell din exercițiul 1 pentru a include o variabilă. Variabila va deține conținutul mesajului "Bine ati venit!"
-EXERCITIU
-
-TEXT=”Bine ati venit!”
-echo $TEXT
