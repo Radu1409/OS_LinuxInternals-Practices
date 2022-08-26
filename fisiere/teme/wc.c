b2245cd470157b41a5661d0a6ea510c409f6ed54
Purecel Radu <79523935+Radu1409@users.noreply.github.com>
2022-08-26 17:02:28 +0300

    Imbunatatire `wc.c`

--- fisiere/teme/wc.c
+++ fisiere/teme/wc.c
@@ -23,8 +23,6 @@ typedef struct {
     size_t size;
 } info_t;
 
-void write_stdin(int fd);
-
 /* display option flags, lines, words, bytes */
 bool lFlag = 0;
 bool wFlag = 0;
@@ -44,6 +42,8 @@ int main(int argc, char** argv)
     size_t *sizes; // vector with counted values
     size_t *size;  // pointer for cycling through sizes
     int i, j;
+    int byte_num;
+    int fd = STDIN_FILENO;
     bool found_error = false;
     
     // check for optional arguments
@@ -77,60 +77,60 @@ int main(int argc, char** argv)
         cFlag = 1;
     }
     
-    if (numFiles < 1)
-    {
-        write_stdin(STDIN_FILENO);
-    }
-
     numFields = wFlag + lFlag + cFlag + LFlag;
 
     // allocate memory for the counts
-    files = malloc(sizeof(info_t) * (numFiles + (numFiles > 1)));
-    sizes = malloc(sizeof(size_t) * numFields * (numFiles + (numFiles > 1)));
-
+    if (numFiles > 0)
+    {
+        files = malloc(sizeof(info_t) * (numFiles + (numFiles > 1)));
+        sizes = malloc(sizeof(size_t) * numFields * (numFiles + (numFiles > 1)));
+    }
+    else
+        sizes = malloc(sizeof(size_t) * numFields);
+	
     // go through all the files
-    for(i=optind, file=files, size=sizes; i<argc; i++, file++)
+    for(i=(numFiles > 0) ? optind : 0, file=(numFiles > 0) ? files : NULL, size=sizes; i<argc; i++, (numFiles > 0) ? file++ : NULL)
     {
         int byte_count=0;
         int word_count = 0;
         int line_count = 0;
         char previous[10]; // TODO: this is strange
-        int fd;
         int max_length = -1;
         int curr_line_char = 0;
 
         previous[0]=' ';
 
-        file->name = argv[i];
-        if((fd=open(file->name,O_RDONLY)) < 0 )
-        {
-            file->error = errno;
-            file->size = 0;
-            file->type = F_NORMAL;
-            if (lFlag) *size++ = 0;
-            if (wFlag) *size++ = 0;
-            if (cFlag) *size++ = 0;
-            if (LFlag) *size++ = 0;
-            found_error = true;
-            continue;
-        }
-        else
-        {
-            fstat(fd, &st);
-
-            if(S_ISDIR(st.st_mode))
+        if (numFiles > 0)
+	{
+            file->name = argv[i];
+            if((fd=open(file->name,O_RDONLY)) < 0 )
             {
+                file->error = errno;
                 file->size = 0;
-                file->type = F_DIR;
+                file->type = F_NORMAL;
+                if (lFlag) *size++ = 0;
+                if (wFlag) *size++ = 0;
+                if (cFlag) *size++ = 0;
+                if (LFlag) *size++ = 0;
+                found_error = true;
+                continue;
             }
             else
             {
-                file->size = st.st_size;
-                file->type = F_NORMAL;
+                fstat(fd, &st);
+
+                if(S_ISDIR(st.st_mode))
+                {
+                    file->size = 0;
+                    file->type = F_DIR;
+                }
+                else
+                {
+                    file->size = st.st_size;
+                    file->type = F_NORMAL;
+                }
             }
-
-        }
-
+	}
         while(read(fd,buffer,1) >0)
         {
             // TODO: some of the counts in binary files are wrong
@@ -161,8 +161,10 @@ int main(int argc, char** argv)
         if (cFlag) *size++ = byte_count;
         // save max length ignoring last newline character
         if (LFlag) *size++ = max_length - 1;
-
-        close(fd);
+        
+        byte_num = byte_count;
+        if (numFiles > 0) close(fd);
+        if (numFiles < 1) break;
     }
 
     // count the totals
@@ -198,134 +200,80 @@ int main(int argc, char** argv)
     // when displaying only one value do not align at all
     if (numFields > 1 || numFiles > 1)
     {
-        width = snprintf(NULL, 0, "%ld", files[numFiles - 1].size);
-        // when there is at least one directory among the files, align at least 7 characters
-        for (i = 0; i < numFiles; i++)
-            if  (files[i].type == F_DIR && width < 7)
-                width = 7;
+        if (numFiles < 1)
+            width = snprintf(NULL, 0, "%d", byte_num);
+        else
+	{
+            width = snprintf(NULL, 0, "%ld", files[numFiles - 1].size);
+            // when there is at least one directory among the files, align at least 7 characters
+            for (i = 0; i < numFiles; i++)
+                if  (files[i].type == F_DIR && width < 7)
+                    width = 7;
+	}
     }
 
     // display the counts
-    for (i = 0, size = sizes, file=files; i < numFiles; i++, size += numFields, file++)
+    if (numFiles < 1)
     {
-        if (file->error)
+        size = sizes;
+        fstat(fd, &st);
+
+        if(S_ISDIR(st.st_mode))
         {
-            errno = file->error;
-            err_ret("%s", file->name);
-            continue;
-        }
-        if (file->type == F_DIR)
-        {
-            err_msg("%s: Is a directory", files->name);
+            err_msg("'standard input': Is a directory");
+            width = 7;
             found_error = true;
         }
-
-        for (j = 0; j < numFields; j++)
-            printf("%*ld ", width, size[j]);
-        printf("%s\n", file->name);
-    }
-
-    free(sizes);
-    free(files);
-
-    if(found_error)
-        exit(1);
-}
-
-void write_stdin(int fd)
-{
-    int line_no = 0;
-    int byte_no = 0;
-    int word_no = 0;
-    int max_length = -1;
-    int width = 0;
-    int status = 0;
-    int curr_line_char = 0;
-    size_t *sizes;
-    size_t *size;
-    char buf[10];
-    char previous[10];
-
-    previous[0]=' ';
-    struct stat st;
-
-    int Flag = lFlag + wFlag + cFlag + LFlag;
-    sizes = malloc(sizeof(size_t) * Flag);
-
-    while (read(fd,buf,1) > 0)
-    {
-        // TO DO: some of the counts in binary files are wrong
-        byte_no++;
-        // keep the count of characters per line
-        // this will reset for newline
-        curr_line_char++;
-
-        if(buf[0]=='\n')
+        if (isatty(STDIN_FILENO))
         {
-            line_no++;
-            // max_length will store longest line size
-            // if current line char count is greter, store it in max length
-            if(LFlag && curr_line_char > max_length)
-                max_length = curr_line_char;
-
-            // reset per line count
-            curr_line_char = 0;
-        }
-
-        if(isspace(buf[0])==0 && isspace(previous[0])!=0)
-            word_no++;
-
-        previous[0]=buf[0];
-    }
-    size = sizes;
-
-    if (lFlag) *size++ = line_no;
-    if (wFlag) *size++ = word_no;
-    if (cFlag) *size++ = byte_no;
-    if (LFlag) *size++ = max_length - 1;
-        
-	
-    if (Flag > 1)
-    {
-        width = snprintf(NULL, 0, "%d", byte_no);
-    }
-    size = sizes;
-    fstat(fd, &st);
-
-    if(S_ISDIR(st.st_mode))
-    {
-        err_msg("'standard input': Is a directory");
-        width = 7;
-        status = 1;
-    }
-
-    // display the counts    
-    if (isatty(STDIN_FILENO))
-    {
-        for (int j = 0; j < Flag; j++)
-        {
-            if (Flag == 1)
+            for (int j = 0; j < numFields; j++)
             {
-                printf("%ld", size[j]);
-                break;
+                if (numFields == 1)
+                {
+                    printf("%ld", size[j]);
+                    break;
+                }
+                if (j != numFields - 1)
+                    printf("%*ld ", 7, size[j]);
+                else
+                    printf("%*ld", 7, size[j]);
             }
-            if (j != Flag - 1)
-                printf("%*ld ", 7, size[j]);
-            else
-                printf("%*ld", 7, size[j]);
         }
+        else
+        {
+            for (int j = 0; j < numFields; j++)
+                if (j != numFields - 1)
+                    printf("%*ld ", width, size[j]);
+                else
+                    printf("%*ld", width, size[j]);
+        }	    
+        printf("\n");
+        free(sizes);
+        close(fd);
     }
     else
     {
-        for (int j = 0; j < Flag; j++)
-            if (j != Flag - 1)
-                printf("%*ld ", width, size[j]);
-            else
-                printf("%*ld", width, size[j]);
-    }	    
-    printf("\n");
-    free(sizes);
+        for (i = 0, size = sizes, file=files; i < numFiles; i++, size += numFields, file++)
+        {
+            if (file->error)
+            {
+                errno = file->error;
+                err_ret("%s", file->name);
+                continue;
+            }
+            if (file->type == F_DIR)
+            {
+                err_msg("%s: Is a directory", files->name);
+                found_error = true;
+            }
 
-    close(fd);
-    exit(status);
+            for (j = 0; j < numFields; j++)
+                printf("%*ld ", width, size[j]);
+            printf("%s\n", file->name);
+        }
+        free(sizes);
+        free(files);
+    }
+    if(found_error)
+        exit(1);
 }
